ed53445c53b631bbbc5f6a472dcc732d9d2916c6
lubna-thomas <lubz.thomas@gmail.com>
2022-03-18 18:03:28 +0000

    body parser increased to 50

--- v12/app.js
+++ v12/app.js
@@ -13,8 +13,9 @@ app.set('view engine', 'hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json({limit:  '10mb'}));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
+app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
