5a1a8593901e3a3d506ee5e0497796d0d539486a
lubna-thomas <lubz.thomas@gmail.com>
2022-03-18 19:55:29 +0000

    code original

--- v12/app.js
+++ v12/app.js
@@ -13,10 +13,8 @@ app.set('view engine', 'hbs');
 
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({limit: '500mb'}));
-app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
