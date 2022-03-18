99ecd6cd44146e5239d9ce6c991078e30987f17d
lubna-thomas <lubz.thomas@gmail.com>
2022-03-18 19:53:12 +0000

    500 mb change

--- v12/app.js
+++ v12/app.js
@@ -15,8 +15,8 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 //app.use(bodyParser.json());
 //app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.use(bodyParser.json({limit: '500mb'}));
+app.use(bodyParser.urlencoded({limit: '500mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
