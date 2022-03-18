d3d3dd0ff78a8534419c75a7d72b0e7e8b38b08e
lubna-thomas <lubz.thomas@gmail.com>
2022-03-18 18:06:51 +0000

    removed express

--- v12/app.js
+++ v12/app.js
@@ -15,7 +15,6 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: false }));
-app.use(express.json());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
