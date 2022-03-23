b7155d953f134766fbe938fbf5c6a5cdd57380ba
lubna-thomas <lubz.thomas@gmail.com>
2022-03-23 09:45:44 +0000

    url encoded was missing

--- v12/app.js
+++ v12/app.js
@@ -15,7 +15,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 //app.use(bodyParser.json());
 app.use(bodyParser.json({ limit: '10mb' }));
-//app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: false }));
 //app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
